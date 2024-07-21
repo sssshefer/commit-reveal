eda7b2a9efd12384316f53347ed2ebe415ec3bc4
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-07-21 04:47:05 +0300

    Commit and reveal functionality

--- contracts/CommitReveal.sol
+++ contracts/CommitReveal.sol
@@ -8,11 +8,13 @@ contract CommitReveal {
     address[] public candidates;
     mapping(address => bool) public isCandidate;
     mapping(address => bytes32) public commits;
-    bool votingStatus;
+    mapping(address => uint) public votesCount;
+
+    bool votingIsActive;
 
     constructor(address[] memory _candidates) {
         require(_candidates.length >= 2, "Minimum number of candidates is 2");
-
+        
         for (uint256 i = 0; i < _candidates.length; i++) {
             require(
                 _candidates[i] != address(0),
@@ -21,5 +23,24 @@ contract CommitReveal {
             require(!isCandidate[_candidates[i]], "Duplicated candidate");
             isCandidate[_candidates[i]] = true;
         }
+    }
+
+    function commitVote(bytes32 _hashedVoteData) external {
+        require(votingIsActive, "Voting is not active");
+        require(commits[msg.sender] == bytes32(0), "You have already voted");
+
+        commits[msg.sender] = _hashedVoteData;
+    }
+
+    function revealVote(address candidate, bytes32 _secret) external {
+        require(isCandidate[candidate], "No such candidate");
+        bytes32 commit = keccak256(abi.encode(candidate, _secret, msg.sender));
+        require(commits[msg.sender] == commit);
+        votesCount[candidate]++;
+    }
+
+    function stopVoring() external {
+        require(votingIsActive, "You cannot stop inactive voting");
+        votingIsActive = false;
     }
 }
